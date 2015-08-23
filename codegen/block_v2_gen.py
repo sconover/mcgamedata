82f77d5844c02fdc6446d41e53fa8c06417f2d23
Steve Conover <steve@squareup.com>
2015-08-23 08:59:21 -0700

    python standard spacing

--- codegen/block_v2_gen.py
+++ codegen/block_v2_gen.py
@@ -13,90 +13,90 @@ target_definition_file = os.path.join(this_dir, "../mcgamedata/block_definition.
 target_block_file = os.path.join(this_dir, "../mcgamedata/block.py")
 
 block_definition_lines = [
-  "from block_property import EnumProperty, IntegerProperty, BooleanProperty",
-  "",
-  "class BlockDefinition():",
-  "  pass",
-  ""
+    "from block_property import EnumProperty, IntegerProperty, BooleanProperty",
+    "",
+    "class BlockDefinition():",
+    "    pass",
+    ""
 ]
 
 # {
-#    "id": 145,
-#    "name": "minecraft:anvil",
-#    "properties": [
-#      {
-#        "type": "integer",
-#        "name": "damage",
-#        "value": 0,
-#        "possible_values": [
-#          0,
-#          1,
-#          2
+#        "id": 145,
+#        "name": "minecraft:anvil",
+#        "properties": [
+#            {
+#                "type": "integer",
+#                "name": "damage",
+#                "value": 0,
+#                "possible_values": [
+#                    0,
+#                    1,
+#                    2
+#                ]
+#            },
+#            {
+#                "type": "enum",
+#                "name": "facing",
+#                "value": "north",
+#                "possible_values": [
+#                    "north",
+#                    "south",
+#                    "west",
+#                    "east"
+#                ]
+#            }
 #        ]
-#      },
-#      {
-#        "type": "enum",
-#        "name": "facing",
-#        "value": "north",
-#        "possible_values": [
-#          "north",
-#          "south",
-#          "west",
-#          "east"
-#        ]
-#      }
-#    ]
-#  },
+#    },
 
 all_blocks = []
 
 for block in blocks:
-  block_id = block["id"]
-  lowercase_block_name = block["name"].replace("minecraft:", "")
-  uppercase_block_name = lowercase_block_name.upper()
-  upper_camel_block_name = string.capwords(lowercase_block_name, "_").replace("_", "")
+    block_id = block["id"]
+    lowercase_block_name = block["name"].replace("minecraft:", "")
+    uppercase_block_name = lowercase_block_name.upper()
+    upper_camel_block_name = string.capwords(lowercase_block_name, "_").replace("_", "")
 
-  all_blocks.append({'upper':uppercase_block_name, 'camel':upper_camel_block_name})
+    all_blocks.append({'upper':uppercase_block_name, 'camel':upper_camel_block_name})
 
-  block_definition_lines.extend([
-    "",
-    "class " + upper_camel_block_name + "(BlockDefinition):",
-    INDENT + "def __init__(self):",
-    INDENT + INDENT + "self.id = " + str(block_id),
-    INDENT + INDENT + "self.name = '" + lowercase_block_name + "'",
-    ""
-  ])
+    block_definition_lines.extend([
+        "",
+        "class " + upper_camel_block_name + "(BlockDefinition):",
+        INDENT + "def __init__(self):",
+        INDENT + INDENT + "self.id = " + str(block_id),
+        INDENT + INDENT + "self.name = '" + lowercase_block_name + "'",
+        ""
+    ])
 
-  for p in block["properties"]:
-    lowercase_property_name = p["name"]
-    uppercase_property_name = lowercase_property_name.upper()
-    capitalized_type = string.capwords(p["type"])
+    for p in block["properties"]:
+        lowercase_property_name = p["name"]
+        uppercase_property_name = lowercase_property_name.upper()
+        capitalized_type = string.capwords(p["type"])
 
-    block_definition_lines.append(
-      INDENT + "PROPERTY_" + uppercase_property_name + " = " +
-        capitalized_type + "Property('" + lowercase_property_name + "')")
+        block_definition_lines.append(
+            INDENT + "PROPERTY_" + uppercase_property_name + " = " +
+                capitalized_type + "Property('" + lowercase_property_name + "')")
 
-    property_value_lines = []
-    max_property_value_name_length = 0
+        property_value_lines = []
+        max_property_value_name_length = 0
 
-    for v in p["possible_values"]:
-      uppercase_value = str(v).upper()
-      const_name = value_line = INDENT + uppercase_property_name + "_" + uppercase_value
-      value = "PROPERTY_" + uppercase_property_name + ".value("
-      if type(v) is types.StringType or type(v) is types.UnicodeType:
-        value += "'" + v + "'"
-      else:
-        value += str(v)
-      value += ")"
-      property_value_lines.append([const_name, value])
-      if len(const_name) > max_property_value_name_length:
-        max_property_value_name_length = len(const_name)
+        for v in p["possible_values"]:
+            uppercase_value = str(v).upper()
+            const_name = value_line = INDENT + uppercase_property_name + "_" + uppercase_value
+            value = "PROPERTY_" + uppercase_property_name + ".value("
+            if type(v) is types.StringType or type(v) is types.UnicodeType:
+                value += "'" + v + "'"
+            else:
+                value += str(v)
+            value += ")"
+            property_value_lines.append([const_name, value])
+            if len(const_name) > max_property_value_name_length:
+                max_property_value_name_length = len(const_name)
 
-    for line in property_value_lines:
-      value_line = line[0].ljust(max_property_value_name_length) + " = " + line[1]
-      block_definition_lines.append(value_line)
+        for line in property_value_lines:
+            value_line = line[0].ljust(max_property_value_name_length) + " = " + line[1]
+            block_definition_lines.append(value_line)
 
-    block_definition_lines.append("")
+        block_definition_lines.append("")
 
 body = "\n".join(block_definition_lines) + "\n"
 print body
@@ -105,20 +105,20 @@ f.write(body)
 f.close()
 
 block_lines = [
-  "import block_definition",
-  "",
-  "ALL = []",
-  ""
+    "import block_definition",
+    "",
+    "ALL = []",
+    ""
 ]
 
 max_block_name_length = 0
 for block in all_blocks:
-  if len(block['upper']) > max_block_name_length:
-    max_block_name_length = len(block['upper'])
+    if len(block['upper']) > max_block_name_length:
+        max_block_name_length = len(block['upper'])
 
 for block in all_blocks:
-  block_lines.append(block['upper'].ljust(max_block_name_length) + " = block_definition." + \
-    block['camel'] + "(); ALL.append(" + block['upper'] + ")")
+    block_lines.append(block['upper'].ljust(max_block_name_length) + " = block_definition." + \
+        block['camel'] + "(); ALL.append(" + block['upper'] + ")")
 
 body = "\n".join(block_lines) + "\n"
 print body
--- codegen/living_gen.py
+++ codegen/living_gen.py
@@ -11,15 +11,15 @@ import types
 
 
 # might be worth building up to these methods...and teach iteration.
-#   having a whole bunch of animals sit and stand makes an impression
+#     having a whole bunch of animals sit and stand makes an impression
 
 # for thing in my_path():
-#    if type(thing) == living.OCELOT:
-#        start_task(living.OCELOT.FOLLOW_OWNER, target=thing) # type check - server error if can't apply this...
+#        if type(thing) == living.OCELOT:
+#                start_task(living.OCELOT.FOLLOW_OWNER, target=thing) # type check - server error if can't apply this...
 
 # for thing in my_path():
-#    if type(thing) == living.OCELOT:
-#        reset_task(living.OCELOT.FOLLOW_OWNER, target=thing)
+#        if type(thing) == living.OCELOT:
+#                reset_task(living.OCELOT.FOLLOW_OWNER, target=thing)
 
 # pen_down(living.OCELOT)
 # start_task(living.ANY.FOLLOW_OWNER) # implicit select of things that have this task
@@ -34,10 +34,10 @@ import types
 
 
 # TODO: generate markdown docs
-#   transform to ipython
+#     transform to ipython
 
 # TODO: do something similar for entities
-#   enumerate entity tasks...
+#     enumerate entity tasks...
 # entity.startTask(uuid (or array...?), tasks.OCELOT_SIT)
 # entity.startTask(uuid, tasks.OCELOT_FOLLOW_OWNER)
 # entity.stopTask(uuid, tasks.OCELOT_FOLLOW_OWNER)
@@ -73,76 +73,76 @@ target_definition_file = os.path.join(this_dir, "../mcgamedata/living_definition
 target_living_file = os.path.join(this_dir, "../mcgamedata/living.py")
 
 living_definition_lines = [
-  "class LivingDefinition():",
-  "  pass",
-  "",
-  "class EntityTask():",
-  INDENT + "def __init__(self, entity_name, task_name):",
-  INDENT + INDENT + "self.entity_name = entity_name",
-  INDENT + INDENT + "self.task_name = task_name",
-  ""
+    "class LivingDefinition():",
+    "    pass",
+    "",
+    "class EntityTask():",
+    INDENT + "def __init__(self, entity_name, task_name):",
+    INDENT + INDENT + "self.entity_name = entity_name",
+    INDENT + INDENT + "self.task_name = task_name",
+    ""
 ]
 
-  # {
-  #   "name": "ocelot",
-  #   "tasks": [
-  #     "avoid",
-  #     "swim",
-  #     "sit",
-  #     "tempt",
-  #     "follow_owner",
-  #     "ocelot_sit",
-  #     "leap_at_target",
-  #     "ocelot_attack",
-  #     "mate",
-  #     "wander",
-  #     "watch_closest"
-  #   ]
-  # }
+    # {
+    #     "name": "ocelot",
+    #     "tasks": [
+    #         "avoid",
+    #         "swim",
+    #         "sit",
+    #         "tempt",
+    #         "follow_owner",
+    #         "ocelot_sit",
+    #         "leap_at_target",
+    #         "ocelot_attack",
+    #         "mate",
+    #         "wander",
+    #         "watch_closest"
+    #     ]
+    # }
 
 all_living = []
 all_tasks = []
 
 for living in livings:
-  for task in living["tasks"]:
-    if task not in all_tasks:
-      all_tasks.append(task)
+    for task in living["tasks"]:
+        if task not in all_tasks:
+            all_tasks.append(task)
 
 all_tasks = sorted(all_tasks)
 livings.insert(0, {"name": "any", "tasks": all_tasks})
 
 for living in livings:
-  lowercase_living_name = living["name"]
-  uppercase_living_name = lowercase_living_name.upper()
-  upper_camel_living_name = string.capwords(lowercase_living_name, "_").replace("_", "")
+    lowercase_living_name = living["name"]
+    uppercase_living_name = lowercase_living_name.upper()
+    upper_camel_living_name = string.capwords(lowercase_living_name, "_").replace("_", "")
 
-  all_living.append({'upper':uppercase_living_name, 'camel':upper_camel_living_name})
+    all_living.append({'upper':uppercase_living_name, 'camel':upper_camel_living_name})
 
-  living_definition_lines.extend([
-    "",
-    "class " + upper_camel_living_name + "(LivingDefinition):",
-    INDENT + "def __init__(self):",
-    INDENT + INDENT + "self.name = '" + lowercase_living_name + "'",
-    ""
-  ])
+    living_definition_lines.extend([
+        "",
+        "class " + upper_camel_living_name + "(LivingDefinition):",
+        INDENT + "def __init__(self):",
+        INDENT + INDENT + "self.name = '" + lowercase_living_name + "'",
+        ""
+    ])
 
-  task_value_lines = []
-  max_task_value_name_length = 0
+    task_value_lines = []
+    max_task_value_name_length = 0
 
-  for task in living["tasks"]:
-    lowercase_task_name = task
-    uppercase_task_name = task.upper()
+    for task in living["tasks"]:
+        lowercase_task_name = task
+        uppercase_task_name = task.upper()
 
-    task_value_lines.append([uppercase_task_name, "'" + task + "'"])
-    if len(uppercase_task_name) > max_task_value_name_length:
-      max_task_value_name_length = len(uppercase_task_name)
+        task_value_lines.append([uppercase_task_name, "'" + task + "'"])
+        if len(uppercase_task_name) > max_task_value_name_length:
+            max_task_value_name_length = len(uppercase_task_name)
 
-  for line in task_value_lines:
-    value_line = line[0].ljust(max_task_value_name_length) + " = " + \
-      "EntityTask('" + lowercase_living_name + "' ," + line[1] + ")"
-    living_definition_lines.append(INDENT + value_line)
+    for line in task_value_lines:
+        value_line = line[0].ljust(max_task_value_name_length) + " = " + \
+            "EntityTask('" + lowercase_living_name + "' ," + line[1] + ")"
+        living_definition_lines.append(INDENT + value_line)
 
-  living_definition_lines.append("")
+    living_definition_lines.append("")
 
 body = "\n".join(living_definition_lines) + "\n"
 print body
@@ -151,20 +151,20 @@ f.write(body)
 f.close()
 
 living_lines = [
-  "import living_definition",
-  "",
-  "ALL = []",
-  ""
+    "import living_definition",
+    "",
+    "ALL = []",
+    ""
 ]
 
 max_living_name_length = 0
 for living in all_living:
-  if len(living['upper']) > max_living_name_length:
-    max_living_name_length = len(living['upper'])
+    if len(living['upper']) > max_living_name_length:
+        max_living_name_length = len(living['upper'])
 
 for living in all_living:
-  living_lines.append(living['upper'].ljust(max_living_name_length) + " = living_definition." + \
-    living['camel'] + "(); ALL.append(" + living['upper'] + ")")
+    living_lines.append(living['upper'].ljust(max_living_name_length) + " = living_definition." + \
+        living['camel'] + "(); ALL.append(" + living['upper'] + ")")
 
 body = "\n".join(living_lines) + "\n"
 print body
